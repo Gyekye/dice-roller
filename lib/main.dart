377b59163932423f796c84a2ff2d75aafce8a378
GYEKYE <mrgyekye740@gmail.com>
2021-03-13 10:24:50 +0000

    building UI complete

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,43 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.red,
         appBar: AppBar(
           backgroundColor: Colors.red,
           title: Text('Dice Roller'),
           centerTitle: true,
           elevation: 0.0,
-        )
+        ),
+        body: Center(
+          child: Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FlatButton(
+                    child: Image.asset('assets/images/dice1.png'),
+                    onPressed: () {
+                      print('left');
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FlatButton(
+                    child: Image.asset('assets/images/dice2.png'),
+                    onPressed: () {
+                      print('right');
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
